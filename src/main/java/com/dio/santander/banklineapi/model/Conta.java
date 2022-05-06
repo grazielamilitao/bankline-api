93176863fe34da3b41caf3090205e823d78920dd
Graziela Militão <36998416+grazielamilitao@users.noreply.github.com>
2022-05-06 17:00:11 -0300

    creating model class

---
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -0,0 +1,19 @@
+package com.dio.santander.banklineapi.model;
+
+public class Conta {
+	private Long numero;
+	private double saldo;
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
