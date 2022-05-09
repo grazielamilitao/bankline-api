0eca52be5a778a1430097700e9afeb92c8232874
Graziela Militão <36998416+grazielamilitao@users.noreply.github.com>
2022-05-09 17:42:28 -0300

    fixing mistakes

--- src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.*;
 
 import com.dio.santander.bankline.api.dto.NovaMovimentacao;
 import com.dio.santander.bankline.api.model.*;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
-
 @Service
 public class MovimentacaoService {
 	@Autowired
 	private MovimentacaoRepository repository;
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
 	public void save(NovaMovimentacao novaMovimentacao) {
 		Movimentacao movimentacao = new Movimentacao();
 		Double valor = novaMovimentacao.getTipo() == MovimentacaoTipo.RECEITA ? novaMovimentacao.getValor()
@@ -22,5 +24,12 @@ public class MovimentacaoService {
 		movimentacao.setIdConta(novaMovimentacao.getIdConta());
 		movimentacao.setTipo(novaMovimentacao.getTipo());
 		movimentacao.setValor(novaMovimentacao.getValor());
+		
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if(correntista!=null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo()+valor);
+			correntistaRepository.save(correntista);
+		}
+		repository.save(movimentacao);
 	}
 }
